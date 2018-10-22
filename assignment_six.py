80aabf0c5b59fa1c35a6411244534f9f035f7a51
Yerim <yerim@iMac6.local>
2018-10-22 08:58:03 -0400

    Today I finished my flowchart and started my program

---
+++ assignment_six.py
@@ -0,0 +1,16 @@
+import random
+
+
+def number_simulation():
+    user_simulation = int(input("How many simulations do you wish to run?"))
+    return user_simulation
+
+def birthday_lists():
+
+
+
+def main():
+    
+
+
+main()
