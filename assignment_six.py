57cbeba7731b50e2db04fa5745905bf3eb4745e0
Yerim <yerim@iMac6.local>
2018-10-23 10:16:02 -0400

    Today I described some of my functions and was able to make the program check for the different birthday

--- assignment_six.py
+++ assignment_six.py
@@ -2,15 +2,31 @@ import random
 
 
 def number_simulation():
-    user_simulation = int(input("How many simulations do you wish to run?"))
+    user_simulation = (input("How many simulations do you wish to run?"))
     return user_simulation
 
 def birthday_lists():
+    birthdays = []
+    for i in range(23):
+         birthdays.append(random.randint(1, 365))
+    print(birthdays)
+    return birthdays
+
+
+def duplicate_birthdays(birthdays):
+
+    check_birthdays = set(birthdays)
+    if len(check_birthdays) < len(birthdays):
+        print("Found duplicates")
+    else:
+        print("no duplicates")
+
 
 
 
 def main():
-    
+    birthday_check = birthday_lists()
+    duplicate_birthdays(birthday_check)
 
 
 main()
