51b30ac4f5b9eee0ae5a70d3182d264f76c5a00c
Yerim <yerim@iMac6.local>
2018-10-25 12:06:01 -0400

    Today I finalized all my functions and was able to make the program work effectively. I just need to add the docstrings.

--- assignment_six.py
+++ assignment_six.py
@@ -1,38 +1,52 @@
+# Yerim Dia
+# October 25, 2018
+# Introduction to Computer Science
+
+# This program is based in the Birthday Paradox
+# This program starts off by getting a number of simulations that the user wants to run
+# Then the program generates a lists of 23 birthdays from 1 to 365
+#
+
+
 import random
 
 
 def number_simulation():
-    user_simulation = (input("How many simulations do you wish to run?"))
+    """
+    This function gets the 
+    :return: This returns the number of simulations the user wishes to run
+    """
+    user_simulation = int(input("How many simulations do you wish to run?"))
     return user_simulation
+
 
 def birthday_lists():
     birthdays = []
     for i in range(23):
-         birthdays.append(random.randint(1, 365))
-    print(birthdays)
+            birthdays.append(random.randint(1, 365))
     return birthdays
 
 
 def duplicate_birthdays(birthdays):
+    #   A set deletes all duplicates in the list and then checks
     check_birthdays = set(birthdays)
     if len(check_birthdays) < len(birthdays):
         return True
     else:
-       return False
-
-
+        return False
 
 
 def main():
-    birthday_check = birthday_lists()
-    duplicate_birthdays(birthday_check)
+    user_simulation = number_simulation()
     birthday_match = 0
-    if duplicate_birthdays == True:
-        birthday_match += 1
-    elif duplicate_birthdays == False:
-        print("There were no matches")
-    print(birthday_match)
-
+    for i in range(user_simulation):
+        birthday_check = birthday_lists()
+        duplicate_birthdays(birthday_check)
+        if duplicate_birthdays(birthday_check) == True:
+                birthday_match += 1
+    print("There was", birthday_match, "of the same birthday")
+    birthday_percent = birthday_match / user_simulation * 100
+    print(" This is about", birthday_percent, "% of the time ")
 
 
 main()
